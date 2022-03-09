5ee4614461b002d506d5f0d14d23cc39a73bec35
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-09 16:26:13 +0100

    Export Statements in TypeScript, Defining a Company

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hi there!");
+import { Company } from "./Company";
+import { User } from "./User";
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
